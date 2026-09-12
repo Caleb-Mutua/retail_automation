46311324d9ee66bcdeaf6046a0e92d63b3add70c
cayleb <lemmyxking17@gmail.com>
2026-09-12 10:48:45 +0300

    "Save progress on dev before switching to main"

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Retail Automation',
+    'version': '18.0.1.0.0',
+    'summary': 'Retail automation solution for small businesses',
+
+    'description': 
+     """
+     Retail Automation
+
+     A scalable retail management solution designed to help small
+     businesses manage products, orders, inventory, customers, and
+     sales workflows.
+     """,
+
+    'author': 'Caleb Mutua',
+    'category': 'Sales',
+    'license': 'LGPL-3',
+
+    'depends': ['base',],
+
+    'data': [],
+
+    'demo': [],
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
